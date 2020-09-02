fabc9c816de3bd864aca9d0b4d415c4591ad6384
BasithAbdulM <abdul.basith332@gmail.com>
2020-09-02 16:14:52 +0530

    POM

---
+++ src/test/java/Ddlogintestcases/SignIn.java
@@ -0,0 +1,21 @@
+package Ddlogintestcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class SignIn {
+
+
+    @Test
+    public void in() throws IOException {
+        System.setProperty("webdriver.chrome.driver", "src/driver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://opensource-demo.orangehrmlive.com/index.php/auth/validateCredentials");
+        ExcelDP excelDP=new ExcelDP();
+        excelDP.readexel();
+
+    }
+}
